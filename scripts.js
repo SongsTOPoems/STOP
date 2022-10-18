08b6f587bbca9d946b01b646e3c110e994e6b1f9
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-18 23:12:03 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const animatedNav = document.getElementById("navbar");
+        const animatedCov = document.getElementsByClassName("cov");
+        const windowOffsetTop = window.innerHeight - 60;
+        if (window.scrollY >= windowOffsetTop) {
+            addClass(animatedNav, "navBg");
+        }
+        else {
+            if (animatedNav.hasAttribute('class')) {
+                animatedNav.classList.remove("navBg");
+            }
+        }
+        const a = animatedCov[0];
+        const b = animatedCov[1];
+        if ((window.innerHeight + window.scrollY) >= a.offsetTop) {
+            addClass(a, "slider1");
+        }
+        if ((window.innerHeight + window.scrollY) >= b.offsetTop) {
+            addClass(b, "slider2");
+        }
+    });
+});
+
+function addClass(element, className) {
+    const arrayClasses = element.className.split(" ");
+    if (arrayClasses.indexOf(className) === -1) {
+        element.className += " " + className;
+    }
+}
+
+$(document).ready(function(){
+    $("#start").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#subHero").offset().top
+        }, 800);
+    });
+});
+
+
+/* textShown */ 
+
+function load(file) {
+    $.ajax({
+        method: 'GET',
+        url: file,
+        success: function (d) {
+            $('#file').html(d)
+            $('.show').prop("checked", false)
+            addIds()
+            filltabs()
+        },
+        error: function () {
+            alert('Could not load file ' + file)
+        }
+    });
+}
+
+$(document).ready(function(){
+    $("#text2").click(function(){
+        $("#textShown").load("https://typantheon.github.io/magazine/issues/dyonisus.html");
+    });
+});
