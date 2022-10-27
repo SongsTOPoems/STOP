0453fbeed7d77829c4b100f3e8fe8f41f5c4f49b
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-27 09:16:12 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.addEventListener("scroll", function(event) { 
-        const animatedNav = document.getElementById("navbar");
-        const animatedCov = document.getElementsByClassName("cov");
-        const windowOffsetTop = window.innerHeight - 60;
-        if (window.scrollY >= windowOffsetTop) {
-            addClass(animatedNav, "navBg");
-        }
-        else {
-            if (animatedNav.hasAttribute('class')) {
-                animatedNav.classList.remove("navBg");
-            }
-        }
-        const a = animatedCov[0];
-        const b = animatedCov[1];
-        if ((window.innerHeight + window.scrollY) >= a.offsetTop) {
-            addClass(a, "slider1");
-        }
-        if ((window.innerHeight + window.scrollY) >= b.offsetTop) {
-            addClass(b, "slider2");
-        }
-    });
-});
-
-function addClass(element, className) {
-    const arrayClasses = element.className.split(" ");
-    if (arrayClasses.indexOf(className) === -1) {
-        element.className += " " + className;
-    }
-}
-
-
-
-
 $(document).ready(function(){
     $("#start").click(function() {
         $('html, body').animate({
