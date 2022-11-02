7c5eff3d3a1a3916236d51fed02214ad1a5788de
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-11-02 10:38:34 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $("#projectList").hide();
     $("#start").click(function() {
         $('html, body').animate({
             scrollTop: $("#subHero").offset().top
